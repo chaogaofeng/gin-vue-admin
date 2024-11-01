4465e1e20a018f4687d61e772cdd1bce45e63563
chaogaofeng <23396178+chaogaofeng@users.noreply.github.com>
2024-11-01 23:06:32 +0800

    fix bill plan

--- server/router/swap/billing_plan.go
+++ server/router/swap/billing_plan.go
@@ -5,24 +5,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type BillingPlanRouter struct {}
+type BillingPlanRouter struct{}
 
 // InitBillingPlanRouter 初始化 账单计划 路由信息
-func (s *BillingPlanRouter) InitBillingPlanRouter(Router *gin.RouterGroup,PublicRouter *gin.RouterGroup) {
+func (s *BillingPlanRouter) InitBillingPlanRouter(Router *gin.RouterGroup, PublicRouter *gin.RouterGroup) {
 	billingPlanRouter := Router.Group("billingPlan").Use(middleware.OperationRecord())
-	billingPlanRouterWithoutRecord := Router.Group("billingPlan")
+	//billingPlanRouterWithoutRecord := Router.Group("billingPlan")
 	billingPlanRouterWithoutAuth := PublicRouter.Group("billingPlan")
 	{
-		billingPlanRouter.POST("createBillingPlan", billingPlanApi.CreateBillingPlan)   // 新建账单计划
-		billingPlanRouter.DELETE("deleteBillingPlan", billingPlanApi.DeleteBillingPlan) // 删除账单计划
+		billingPlanRouter.POST("createBillingPlan", billingPlanApi.CreateBillingPlan)             // 新建账单计划
+		billingPlanRouter.DELETE("deleteBillingPlan", billingPlanApi.DeleteBillingPlan)           // 删除账单计划
 		billingPlanRouter.DELETE("deleteBillingPlanByIds", billingPlanApi.DeleteBillingPlanByIds) // 批量删除账单计划
-		billingPlanRouter.PUT("updateBillingPlan", billingPlanApi.UpdateBillingPlan)    // 更新账单计划
+		billingPlanRouter.PUT("updateBillingPlan", billingPlanApi.UpdateBillingPlan)              // 更新账单计划
 	}
 	{
-		billingPlanRouterWithoutRecord.GET("findBillingPlan", billingPlanApi.FindBillingPlan)        // 根据ID获取账单计划
-		billingPlanRouterWithoutRecord.GET("getBillingPlanList", billingPlanApi.GetBillingPlanList)  // 获取账单计划列表
+		billingPlanRouterWithoutAuth.GET("findBillingPlan", billingPlanApi.FindBillingPlan)       // 根据ID获取账单计划
+		billingPlanRouterWithoutAuth.GET("getBillingPlanList", billingPlanApi.GetBillingPlanList) // 获取账单计划列表
 	}
 	{
-	    billingPlanRouterWithoutAuth.GET("getBillingPlanPublic", billingPlanApi.GetBillingPlanPublic)  // 账单计划开放接口
+		billingPlanRouterWithoutAuth.GET("getBillingPlanPublic", billingPlanApi.GetBillingPlanPublic) // 账单计划开放接口
 	}
 }
